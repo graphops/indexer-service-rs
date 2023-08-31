474f4755cead932dff13ce9e52e3c99e9b5f6954
Alexis Asseman <alexis@semiotic.ai>
2023-08-31 13:26:40 -0700

    WIP
    Signed-off-by: Alexis Asseman <alexis@semiotic.ai>

--- service/src/tap_manager.rs
+++ service/src/tap_manager.rs
@@ -7,9 +7,11 @@ use std::{
 };
 
 use alloy_sol_types::Eip712Domain;
-use sqlx::PgPool;
+use ethereum_types::Address;
+use log::error;
+use sqlx::{any, PgPool};
 use tap::{
-    core::{tap_manager::Manager, tap_receipt::ReceiptCheck},
+    core::{adapters::escrow_adapter, tap_receipt::ReceiptCheck},
     escrow_adapter::EscrowAdapter,
     rav_storage_adapter::RAVStorageAdapter,
     receipt_checks_adapter::ReceiptChecksAdapter,
@@ -17,43 +19,121 @@ use tap::{
 };
 use tokio::sync::RwLock;
 
+use crate::allocation_monitor;
+
+type Manager = tap::core::tap_manager::Manager<
+    EscrowAdapter,
+    ReceiptChecksAdapter,
+    ReceiptStorageAdapter,
+    RAVStorageAdapter,
+>;
+
+// TODO: Have this implement the allocation_ids storage and updates. This should also
+//       maintain a hashmap of Monitor instances, keyed by allocation_id.
 #[derive(Clone)]
 struct TapManager {
-    manager:
-        Arc<Manager<EscrowAdapter, ReceiptChecksAdapter, ReceiptStorageAdapter, RAVStorageAdapter>>,
+    inner: Arc<RwLock<TapManagerInner>>,
+    _update_loop_handle: Arc<tokio::task::JoinHandle<()>>,
+}
+
+struct TapManagerInner {
+    managers: Arc<RwLock<HashMap<Address, Manager>>>,
+    eligible_allocations: Arc<RwLock<HashSet<Address>>>,
+    escrow_adapter: EscrowAdapter,
 }
 
 impl TapManager {
     pub fn new(
         pgpool: PgPool,
+        allocation_monitor: allocation_monitor::AllocationMonitor,
         domain_separator: Eip712Domain,
         required_checks: Vec<ReceiptCheck>,
         starting_min_timestamp_ns: u64,
     ) -> Self {
-        let escrow_adapter = EscrowAdapter {
-            _gateway_escrow_storage: Arc::new(RwLock::new(HashMap::new())),
-        };
-        let receipt_checks_adapter = ReceiptChecksAdapter::new(
-            pgpool.clone(),
-            Arc::new(RwLock::new(HashMap::new())),
-            Arc::new(RwLock::new(HashSet::new())),
-            Arc::new(RwLock::new(HashSet::new())),
-        );
-        let rav_storage_adapter = RAVStorageAdapter::new(pgpool.clone(), Address::default())
-            .await
-            .unwrap();
-        let receipt_storage_adapter = ReceiptStorageAdapter::new();
+        let eligible_allocations = Arc::new(RwLock::new(HashSet::new()));
+        let update_loop_handle = tokio::spawn(Self::update_loop(
+            allocation_monitor.clone(),
+            eligible_allocations.clone(),
+        ));
+
+        let escrow_adapter = EscrowAdapter::new();
 
         Self {
-            manager: Arc::new(Manager::new(
-                domain_separator,
-                escrow_adapter,
-                receipt_checks_adapter,
-                rav_storage_adapter,
-                receipt_storage_adapter,
-                required_checks,
-                starting_min_timestamp_ns,
-            )),
+            managers: Arc::new(RwLock::new(HashMap::new())),
+            eligible_allocations,
+            escrow_adapter,
+            _update_loop_handle: Arc::new(update_loop_handle),
+        }
+    }
+
+    async fn update_eligible_allocations(
+        inner: &TapManagerInner,
+    ) -> anyhow::Result<()> {
+        let allocations_monitor_read = inner.allocation_monitor.get_eligible_allocations().await;
+        let mut eligible_allocations_new = HashSet::with_capacity(allocations_monitor_read.len());
+        for allocation in allocations_monitor_read.iter() {
+            if !eligible_allocations_new.insert(allocation.id) {
+                return Err(anyhow::anyhow!(
+                    "Duplicate allocation id: {}",
+                    allocation.id
+                ));
+            }
+        }
+
+        // Remove allocations that are no longer eligible from managers
+        let mut managers_write = inner.managers.write().await;
+        let mut managers_remove = Vec::new();
+        for allocation_id in managers_write.keys() {
+            if !eligible_allocations_new.contains(allocation_id) {
+                managers_remove.push(allocation_id.clone());
+            }
+        }
+        for allocation_id in managers_remove {
+            managers_write.remove(&allocation_id);
+        }
+
+        // Add eligible allocations that are not already in managers
+        for allocation_id in eligible_allocations_new.iter() {
+            if !managers_write.contains_key(allocation_id) {
+                let manager = Manager::new(
+                    allocation_id.clone(),
+                    inner.escrow_adapter.clone(),
+                    ReceiptChecksAdapter::new(
+                        inner.pgpool.clone(),
+                        inner.allocation_id.clone(),
+                        inner.domain_separator.clone(),
+                        inner.required_checks.clone(),
+                    ),
+                    ReceiptStorageAdapter::new(inner.pgpool.clone(), allocation_id.clone()),
+                    RAVStorageAdapter::new(inner.pgpool.clone(), allocation_id.clone()),
+                );
+                managers_write.insert(allocation_id.clone(), manager);
+            }
+        }
+
+        *eligible_allocations.write().await = eligible_allocations_new;
+        Ok(())
+    }
+
+    async fn update_loop(
+        allocation_monitor: allocation_monitor::AllocationMonitor,
+        eligible_allocations: Arc<RwLock<HashSet<Address>>>,
+    ) {
+        let mut watch_receiver = allocation_monitor.subscribe();
+
+        loop {
+            match watch_receiver.changed().await {
+                Ok(_) => {
+                    Self::update_eligible_allocations(&allocation_monitor, &eligible_allocations)
+                        .await;
+                }
+                Err(e) => {
+                    error!(
+                        "Error receiving allocation monitor subscription update: {}",
+                        e
+                    );
+                }
+            }
         }
     }
 }
--- tap/src/escrow_adapter.rs
+++ tap/src/escrow_adapter.rs
@@ -7,8 +7,11 @@ use log::warn;
 use tap_core::adapters::escrow_adapter::EscrowAdapter as EscrowAdapterTrait;
 use tokio::sync::RwLock;
 
+/// This is Arc internally, so it can be cloned and shared between threads.
+#[derive(Clone)]
 pub struct EscrowAdapter {
-    _gateway_escrow_storage: Arc<RwLock<HashMap<Address, u128>>>,
+    gateway_escrow_balance: Arc<RwLock<HashMap<Address, u128>>>,
+    gateway_pending_fees: Arc<RwLock<HashMap<Address, u128>>>,
 }
 
 use thiserror::Error;
@@ -18,23 +21,157 @@ pub enum AdapterError {
     AdapterError { error: String },
 }
 
+// TODO: Implement escrow subgraph polling.
+
+impl EscrowAdapter {
+    pub fn new() -> Self {
+        Self {
+            gateway_escrow_balance: Arc::new(RwLock::new(HashMap::new())),
+            gateway_pending_fees: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    pub async fn is_valid_gateway_id(&self, gateway_id: Address) -> bool {
+        self.gateway_escrow_balance
+            .read()
+            .await
+            .contains_key(&gateway_id)
+    }
+}
+
 #[async_trait]
 impl EscrowAdapterTrait for EscrowAdapter {
     type AdapterError = AdapterError;
 
-    async fn get_available_escrow(&self, _gateway_id: Address) -> Result<u128, Self::AdapterError> {
-        // TODO: Implement retrieval of available escrow from local storage
-        warn!("The TAP escrow adapter is not implemented yet. Do not use this in production!");
-        Ok(u128::MAX)
+    async fn get_available_escrow(&self, gateway_id: Address) -> Result<u128, Self::AdapterError> {
+        let balance = self
+            .gateway_escrow_balance
+            .read()
+            .await
+            .get(&gateway_id)
+            .copied()
+            .ok_or(AdapterError::AdapterError {
+                error: format!(
+                    "Gateway {} not found in escrow balances map, could not get available escrow.",
+                    gateway_id
+                )
+                .to_string(),
+            })?;
+        let fees = self
+            .gateway_pending_fees
+            .read()
+            .await
+            .get(&gateway_id)
+            .copied()
+            .ok_or(AdapterError::AdapterError {
+                error: format!(
+                    "Gateway {} not found in pending fees map, could not get available escrow.",
+                    gateway_id
+                )
+                .to_string(),
+            })?;
+
+        Ok(balance - fees)
     }
 
     async fn subtract_escrow(
         &self,
-        _gateway_id: Address,
-        _value: u128,
+        gateway_id: Address,
+        value: u128,
     ) -> Result<(), Self::AdapterError> {
-        // TODO: Implement subtraction of escrow from local storage
-        warn!("The TAP escrow adapter is not implemented yet. Do not use this in production!");
+        let current_available_escrow = self.get_available_escrow(gateway_id).await?;
+
+        let mut fees_write = self.gateway_pending_fees.write().await;
+
+        let fees = fees_write
+            .get_mut(&gateway_id)
+            .ok_or(AdapterError::AdapterError {
+                error: format!(
+                "Gateway {} not found in pending fees map, could not subtract available escrow.",
+                gateway_id
+            )
+                .to_string(),
+            })?;
+
+        if current_available_escrow < value {
+            return Err(AdapterError::AdapterError {
+                error: format!(
+                    "Gateway {} does not have enough escrow to subtract {} from {}.",
+                    gateway_id, value, *fees
+                )
+                .to_string(),
+            });
+        }
+
+        *fees += value;
+
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_subtract_escrow() {
+        let gateway_id = Address::from_str("0xdeadbeefcafebabedeadbeefcafebabadeadbeef").unwrap();
+        let gateway_escrow_balance = Arc::new(RwLock::new(HashMap::new()));
+        let gateway_pending_fees = Arc::new(RwLock::new(HashMap::new()));
+
+        let adapter = EscrowAdapter {
+            gateway_escrow_balance: gateway_escrow_balance.clone(),
+            gateway_pending_fees: gateway_pending_fees.clone(),
+        };
+
+        gateway_escrow_balance
+            .write()
+            .await
+            .insert(gateway_id, 1000);
+        gateway_pending_fees.write().await.insert(gateway_id, 500);
+
+        adapter
+            .subtract_escrow(gateway_id, 500)
+            .await
+            .expect("Subtract escrow.");
+
+        let available_escrow = adapter
+            .get_available_escrow(gateway_id)
+            .await
+            .expect("Get available escrow.");
+        assert_eq!(available_escrow, 0);
+    }
+
+    #[tokio::test]
+    async fn test_subtract_escrow_overflow() {
+        let gateway_id = Address::from_str("0xdeadbeefcafebabedeadbeefcafebabadeadbeef").unwrap();
+        let gateway_escrow_balance = Arc::new(RwLock::new(HashMap::new()));
+        let gateway_pending_fees = Arc::new(RwLock::new(HashMap::new()));
+
+        let adapter = EscrowAdapter {
+            gateway_escrow_balance: gateway_escrow_balance.clone(),
+            gateway_pending_fees: gateway_pending_fees.clone(),
+        };
+
+        gateway_escrow_balance
+            .write()
+            .await
+            .insert(gateway_id, 1000);
+        gateway_pending_fees.write().await.insert(gateway_id, 500);
+
+        adapter
+            .subtract_escrow(gateway_id, 250)
+            .await
+            .expect("Subtract escrow.");
+
+        assert!(adapter.subtract_escrow(gateway_id, 251).await.is_err());
+
+        let available_escrow = adapter
+            .get_available_escrow(gateway_id)
+            .await
+            .expect("Get available escrow.");
+        assert_eq!(available_escrow, 250);
     }
 }
